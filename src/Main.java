e54068c557842c6a77a10d48f07618dc8534afbf
Георгий <voproshalo@yandex.ru>
2023-03-02 19:38:14 +0300

    доллар евро

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(100 + " рублей в долларах это: " +(100 * (1/75.2)));
+        System.out.println(100 + " рублей в евро это: " +(100 * (1/79.3)));
+    }
+}
